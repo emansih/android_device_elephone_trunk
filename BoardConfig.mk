69a835dd598994332dc2123cb3f1be8ede1c2c6d
Michael Ott <m@ott.cx>
2016-03-03 16:32:08 +0100

    Revert "Changed BoardConfig.mk to use prebuilt kernel"
    This reverts commit b0a4391299e8a5f12521e9a03cc17bf32c5acfa0.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,11 +46,8 @@ BOARD_RAMDISK_OFFSET     := 0x01000000
 #endif
 
 # Try to build the kernel
-#TARGET_KERNEL_SOURCE := kernel/elephone/trunk
-#TARGET_KERNEL_CONFIG := cyanogenmod_trunk_defconfig
-
-# User prebuilt kernel from stock rom for now
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/kernel
+TARGET_KERNEL_SOURCE := kernel/elephone/trunk
+TARGET_KERNEL_CONFIG := cyanogenmod_trunk_defconfig
 
 # Bluetooth 
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
