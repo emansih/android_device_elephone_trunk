f1d5c70c2c22977f31eb33478ade6aa0fe1baa4f
Michael Ott <m@ott.cx>
2016-06-10 16:00:23 +0200

    Set TARGET_BOARD_PLATFORM_VARIANT to make libinit work again
    Change-Id: I23d92c647882e725793e637b81e4c07cf484bc23

--- full_trunk.mk
+++ full_trunk.mk
@@ -21,6 +21,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from Elephone Trunk device
 $(call inherit-product, device/elephone/trunk/device.mk)
 
+TARGET_BOARD_PLATFORM_VARIANT := msm8916
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := trunk
 PRODUCT_NAME := full_trunk
